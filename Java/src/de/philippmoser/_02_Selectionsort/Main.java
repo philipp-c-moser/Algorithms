b86cf37079079c49507d527afe272fac695a926b
Philipp C. Moser <info@philipp-moser.de>
2019-05-12 17:33:10 +0200

    Create SortingList

--- Java/src/de/philippmoser/_02_Selectionsort/Main.java
+++ Java/src/de/philippmoser/_02_Selectionsort/Main.java
@@ -1,11 +1,27 @@
 package de.philippmoser._02_Selectionsort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main
 {
 
     public static void main(String args[])
     {
         System.out.println("02 Selectionsort");
+
+        List<Integer> sortingList = new ArrayList<>();
+
+        sortingList.add(50);
+        sortingList.add(12);
+        sortingList.add(72);
+        sortingList.add(48);
+        sortingList.add(12);
+        sortingList.add(84);
+        sortingList.add(3);
+        sortingList.add(13);
+        sortingList.add(35);
+
     }
 
 }
