0fc6eb392a9ea8e1f5cc400fcd5cf40b99605d51
Philipp C. Moser <info@philipp-moser.de>
2019-05-12 17:46:08 +0200

    Found an easier way to add the values to List of integers

--- Java/src/de/philippmoser/_02_Selectionsort/Main.java
+++ Java/src/de/philippmoser/_02_Selectionsort/Main.java
@@ -1,6 +1,7 @@
 package de.philippmoser._02_Selectionsort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main
@@ -10,18 +11,7 @@ public class Main
     {
         System.out.println("02 Selectionsort");
 
-        List<Integer> sortingList = new ArrayList<>();
-
-        sortingList.add(50);
-        sortingList.add(12);
-        sortingList.add(72);
-        sortingList.add(48);
-        sortingList.add(12);
-        sortingList.add(84);
-        sortingList.add(3);
-        sortingList.add(13);
-        sortingList.add(35);
-
+        List<Integer> sortingList = new ArrayList<>(Arrays.asList(50, 12, 72, 48, 12, 84, 3, 13, 35));
 
         System.out.println(selectionSort(sortingList));
 
