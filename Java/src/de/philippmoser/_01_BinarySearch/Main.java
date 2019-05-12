f19746e841dab4502e6a664d8261f3dacd7e4c89
Philipp C. Moser <info@philipp-moser.de>
2019-05-12 17:11:42 +0200

    Method for proceed the binary search

--- Java/src/de/philippmoser/_01_BinarySearch/Main.java
+++ Java/src/de/philippmoser/_01_BinarySearch/Main.java
@@ -2,8 +2,17 @@ package de.philippmoser._01_BinarySearch;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
 
+        System.out.println("01 Binary Search");
+
+    }
+
+
+    private static Integer binarySearch(int[] list, int item)
+    {
+        return null;
     }
 
 }
