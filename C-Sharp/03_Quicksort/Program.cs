ce91a1c8a0a7750023a3b81749bad67a290373da
Philipp C. Moser <info@philipp-moser.de>
2019-05-14 21:35:21 +0200

    Check if the parameter does contain any items

--- C-Sharp/03_Quicksort/Program.cs
+++ C-Sharp/03_Quicksort/Program.cs
@@ -19,6 +19,12 @@ namespace _03_Quicksort
 
         private static int Count(IEnumerable<int> list)
         {
+            // If the list doesn't contain any items.
+            if (!list.Any())
+            {
+                return 0;
+            }
+
             return 1;
         }
 
