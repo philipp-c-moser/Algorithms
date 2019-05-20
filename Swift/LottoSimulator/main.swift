f1969af7bcf95b8717354d24a2647c0e9f15668a
Philipp C. Moser <dev@philipp-moser.de>
2019-05-20 19:21:17 +0200

    Initialize new Xcode Project

---
+++ Swift/LottoSimulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift
+//
+//  Created by Philipp Moser on 20.05.19.
+//  Copyright © 2019 Philipp C. Moser. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
