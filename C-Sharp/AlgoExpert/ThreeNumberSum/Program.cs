79eb828a3550f234cdf9ed2656f3729af006383a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-01 14:06:18 +0100

    ThreeNumberSum: Add Samle Input and Sample Output for description

--- C-Sharp/AlgoExpert/ThreeNumberSum/Program.cs
+++ C-Sharp/AlgoExpert/ThreeNumberSum/Program.cs
@@ -11,6 +11,9 @@ namespace ThreeNumberSum
         }
 
 
+
+        // Sample Input:    [12, 3, 1, 2, -6, 5, -8, 6], 0
+        // Sample Output:   [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
         public static List<int[]> ThreeNumberSum(int[] array, int targetSum)
         {
 
