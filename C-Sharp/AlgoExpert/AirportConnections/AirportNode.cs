2b5a80a91831db1152df47922c33f471d76ad7bd
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-01 18:32:53 +0100

    AirportConnections: Add new Class AirportNode.cs

---
+++ C-Sharp/AlgoExpert/AirportConnections/AirportNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportConnections
+{
+    public class AirportNode
+    {
+        public string Airport;
+        public List<string> Connections;
+        public bool IsReachable;
+        public List<string> UnreachableConnections;
+    }
+}
--- C-Sharp/AlgoExpert/AirportConnections/Program.cs
+++ C-Sharp/AlgoExpert/AirportConnections/Program.cs
@@ -7,6 +7,9 @@ namespace AirportConnections
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            
         }
     }
 }
