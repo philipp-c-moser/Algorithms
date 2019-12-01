85a1307b702346084194695fae47d428d826e08a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-01 18:44:12 +0100

    AirportConnections: Adding sample Input values for method

--- C-Sharp/AlgoExpert/AirportConnections/Program.cs
+++ C-Sharp/AlgoExpert/AirportConnections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AirportConnections
 {
@@ -6,9 +7,54 @@ namespace AirportConnections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Airport Connections");
+
+            var airports = new List<string> {
+                "BGI",
+                "CDG",
+                "DEL",
+                "DOH",
+                "DSM",
+                "EWR",
+                "EYW",
+                "HND",
+                "ICN",
+                "JFK",
+                "LGA",
+                "LHR",
+                "ORD",
+                "SAN",
+                "SFO",
+                "SIN",
+                "TLV",
+                "BUD"
+            };
+
+            var routes = new List<List<string>> {
+                new List<string> {"DSM", "ORD"},
+                new List<string> {"ORD", "BGI"},
+                new List<string> {"BGI", "LGA"},
+                new List<string> {"SIN", "CDG"},
+                new List<string> {"CDG", "SIN"},
+                new List<string> {"CDG", "BUD"},
+                new List<string> {"DEL", "DOH"},
+                new List<string> {"DEL", "CDG"},
+                new List<string> {"TLV", "DEL"},
+                new List<string> {"EWR", "HND"},
+                new List<string> {"HND", "ICN"},
+                new List<string> {"ICN", "JFK"},
+                new List<string> {"JFK", "LGA"},
+                new List<string> {"EYW", "LHR"},
+                new List<string> {"LHR", "SFO"},
+                new List<string> {"SFO", "SAN"},
+                new List<string> {"SFO", "DSM"},
+                new List<string> {"SAN", "EYW"}
+            };
+
+            var startingAirport = "LGA";
 
 
+            AirportConnection.AirportConnections(airports, routes, startingAirport);
             
         }
     }
