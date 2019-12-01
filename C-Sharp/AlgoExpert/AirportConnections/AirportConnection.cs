40c8c489c3685fbcb7c8b9ea976a520892231449
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-01 19:30:20 +0100

    AirportConnections: Add Method to create airport graphs

--- C-Sharp/AlgoExpert/AirportConnections/AirportConnection.cs
+++ C-Sharp/AlgoExpert/AirportConnections/AirportConnection.cs
@@ -10,10 +10,34 @@ namespace AirportConnections
 
         public static int AirportConnections(List<string> airports, List<List<string>> routes, string startingAirport)
         {
+
+            var airportGraph = CreateAirportGraph(airports, routes);
+
             return 0;
         }
 
 
+        private static Dictionary<string, AirportNode> CreateAirportGraph(List<string> airports, List<List<string>> routes)
+        {
+            var airportGraph = new Dictionary<string, AirportNode>();
+
+            foreach(var airport in airports)
+            {
+                airportGraph.Add(airport, new AirportNode(airport));
+            }
+
+            foreach(var route in routes)
+            {
+                var airport = route[0];
+                var connection = route[1];
+
+                airportGraph[airport].Connections.Add(connection);
+            }
+
+
+
+            return airportGraph;
+        }
 
     }
 }
