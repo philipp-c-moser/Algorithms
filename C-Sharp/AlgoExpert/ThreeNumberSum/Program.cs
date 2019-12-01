9c38a4830362de8b6bf5b5a62ec33c5b4fdff569
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-01 14:35:36 +0100

    ThreeNumberSum: Add Display Method

--- C-Sharp/AlgoExpert/ThreeNumberSum/Program.cs
+++ C-Sharp/AlgoExpert/ThreeNumberSum/Program.cs
@@ -7,7 +7,16 @@ namespace ThreeNumberSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ThreeNumberSum");
+            
+            int[] inputValues = { 12, 3, 1, 2, -6, 5, -8, 6 };
+            var targetSum = 0;
+
+            var result = ThreeNumberSum(inputValues, targetSum);
+
+            DisplayThreeNumberSum(result);
+            
+
         }
 
 
@@ -16,7 +25,30 @@ namespace ThreeNumberSum
         // Sample Output:   [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
         public static List<int[]> ThreeNumberSum(int[] array, int targetSum)
         {
+            var triplets = new List<int[]>();          
+            
+            Array.Sort(array);
 
+
+
+            return triplets;
+        }
+
+
+        private static void DisplayThreeNumberSum(List<int[]> input)
+        {
+            foreach (var triple in input)
+            {
+                var currentTriple = new System.Text.StringBuilder();
+                currentTriple.Append("[");
+                foreach (var tripleItem in triple)
+                {
+                    currentTriple.Append(" " + tripleItem + " ");
+                }
+                currentTriple.Append("]");
+
+                Console.WriteLine("TripletItem: " + currentTriple.ToString());
+            }
         }
 
     }
